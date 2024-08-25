d195aa10713e660209361cdf39a2e79e280479dd
atulpatra28 <atulpatra28@gmail.com>
2024-08-25 17:44:27 +0530

    83

--- 83.py
+++ 83.py
@@ -8,7 +8,7 @@ class Solution:
         curr=head
         while(curr and curr.next):
             if curr.val==curr.next.val:
-                curr.next=curr.next.next
+                curr.next=curr.next.next 
 
             else :
                 curr=curr.next
